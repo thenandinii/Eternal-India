377b92b3bc45aaf60f0a17f2623501402e7caa05
thenandinii <parasharnandini15@gmail.com>
2026-01-09 10:50:17 +0530

    Initial commit Eternal India

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          '/images/icon-192x192.png',
+          '/images/icon-512x512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
